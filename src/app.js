aac1ba1e42b15802801313648d21cece45fc7ae9
ruaxin <qy.xin@foxmail.com>
2020-03-05 13:00:42 +0800

    用chai进行单元测试

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
+import chai from 'chai'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
+Vue.component('g-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
@@ -12,4 +15,82 @@ new Vue({
     loading2: true,
     loading3: false,
   }
-}) 
+})
+
+const expect = chai.expect
+{
+  const Construcor = Vue.extend(Button)
+  const vm = new Construcor({
+    propsData: {
+      icon: 'setting'
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-setting')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const Construcor = Vue.extend(Button)
+  const vm = new Construcor({
+    propsData: {
+      icon: 'setting',
+      loading: true
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Construcor = Vue.extend(Button)
+  const vm = new Construcor({
+    propsData: {
+      icon: 'setting'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Construcor = Vue.extend(Button)
+  const vm = new Construcor({
+    propsData: {
+      icon: 'setting',
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const Construcor = Vue.extend(Button)
+  const vm = new Construcor({
+    propsData: {
+      icon: 'setting'
+    }
+  })
+  vm.$mount()
+  vm.$on('click', function () {
+    expect(1).to.eq(1)
+  })
+  let button = vm.$el
+  button.click()
+}
