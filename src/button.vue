57de8a434d46a68cee60ba8f22890da9cbd4b111
ruaxin <qy.xin@foxmail.com>
2020-03-05 11:55:18 +0800

    添加click事件

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,40 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')">
+    <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+    <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
+    <div class="content">
+      <slot/>
+    </div>
+  </button>
 </template>
 <script>
-export default {}
+export default {
+  props: {
+    icon: {},
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator (value) {
+        return value === 'left' || value === 'right'
+      }
+    }
+  }
+}
 </script>
 <style lang="scss">
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
   .g-button {
     font-size: var(--font-size);
     height: var(--button-height);
@@ -12,14 +42,46 @@ export default {}
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+
     &:hover {
       border-color: var(--border-color-hover);
     }
+
     &:active {
       background-color: var(--button-active-bg);
     }
+
     &:focus {
       outline: none;
+    }
+
+    > .content {
+      order: 2;
+    }
+
+    > .icon {
+      order: 1;
+      margin-right: .1em;
+    }
+
+    &.icon-right {
+      > .content {
+        order: 1;
+      }
+
+      > .icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: .1em;
+      }
+    }
+
+    .loading {
+      animation: spin 2s infinite linear;
     }
   }
 </style>
