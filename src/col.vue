d4e3cd2d0c67999239cfb1cfcb26578577e5dccc
ruaxin <qy.xin@foxmail.com>
2020-03-06 20:35:34 +0800

    添加row的align=left/right/center 属性

--- src/col.vue
+++ src/col.vue
@@ -1,9 +1,6 @@
 <template>
-  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-       :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
-    <div style="border: 1px solid green; height: 100px;">
-      <slot></slot>
-    </div>
+  <div class="col" :class="colClass" :style="colStyle">
+    <slot></slot>
   </div>
 </template>
 
@@ -22,13 +19,27 @@ export default {
     return {
       gutter: 0
     }
+  },
+  computed: {
+    colClass () {
+      let { span, offset } = this
+      return [
+        span && `col-${span}`,
+        offset && `offset-${offset}`
+      ]
+    },
+    colStyle () {
+      let { gutter } = this
+      return {
+        paddingLeft: gutter / 2 + 'px', paddingRight: gutter / 2 + 'px'
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
   .col {
-    width: 50%;
     $class-prefix: col-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
