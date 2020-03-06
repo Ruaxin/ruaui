d903217431da6a09c20d4794c8e70a8a384efff1
ruaxin <qy.xin@foxmail.com>
2020-03-06 18:30:47 +0800

    完成row和col的基本功能

--- src/app.js
+++ src/app.js
@@ -3,16 +3,16 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
-import Row from  './row'
-import Col from  './col'
+import Row from './row'
+import Col from './col'
 
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
-Vue.component('g-row',Row)
-Vue.component('g-col',Col)
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
 
 new Vue({
   el: '#app',
@@ -20,12 +20,8 @@ new Vue({
     loading1: false,
     loading2: true,
     loading3: false,
-    message:'hi'
+    message: 'hi'
   },
-  created(){
-  },
-  methods:{
-  }
 })
 
 // import chai from 'chai'
