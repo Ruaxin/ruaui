ead3508c1e44437106996665c18b9ef4dad59b01
ruaxin <qy.xin@foxmail.com>
2020-03-13 15:27:46 +0800

    添加toast和plugin

--- src/app.js
+++ src/app.js
@@ -7,10 +7,11 @@ import Row from './row'
 import Col from './col'
 import Layout from './layout'
 import Header from './header'
-import Sider from  './sider'
-import Content from  './content'
-import Footer from  './footer'
-
+import Sider from './sider'
+import Content from './content'
+import Footer from './footer'
+import Toast from './toast'
+import plugin from './plugin'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -18,11 +19,13 @@ Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
 Vue.component('g-row', Row)
 Vue.component('g-col', Col)
-Vue.component('g-layout',Layout)
-Vue.component('g-header',Header)
-Vue.component('g-sider',Sider)
-Vue.component('g-content',Content)
-Vue.component('g-footer',Footer)
+Vue.component('g-layout', Layout)
+Vue.component('g-header', Header)
+Vue.component('g-sider', Sider)
+Vue.component('g-content', Content)
+Vue.component('g-footer', Footer)
+Vue.component('g-toast', Toast)
+Vue.use(plugin)
 
 new Vue({
   el: '#app',
@@ -32,27 +35,9 @@ new Vue({
     loading3: false,
     message: 'hi'
   },
+  methods: {
+    showToast () {
+      this.$toast('我是message')
+    }
+  }
 })
-
-// import chai from 'chai'
-//
-// const expect = chai.expect
-// // input单元测试
-// {
-//   const Constructor = Vue.extend(Input)
-//   const vm = new Constructor({
-//     propsData: {
-//       error: '你错了'
-//     }
-//   })
-//   vm.$mount()
-//   let useElement = vm.$el.querySelector('use')
-//   let href = useElement.getAttribute('xlink:href')
-//   console.log(vm.$el)
-//   expect(href).to.eq('#i-error')
-//   const errorMessage = vm.$el.querySelector('.errorMessage')
-//   console.log(errorMessage)
-//   expect(errorMessage.innerText).to.equal('你错了')
-//   vm.$el.remove()
-//   vm.$destroy()
-// }
