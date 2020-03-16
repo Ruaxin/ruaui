4018088173172c2239b050c5089e81c2dcdfac00
ruaxin <qy.xin@foxmail.com>
2020-03-16 17:55:10 +0800

    样式基本完成

--- src/tabs.vue
+++ src/tabs.vue
@@ -22,6 +22,24 @@ export default {
       }
     }
   },
+  methods: {
+    checkChildren () {
+      if (this.$children.length === 0) {
+        console && console.warn && console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
+      }
+    },
+    selectTab () {
+      this.$children.forEach((vm) => {
+        if (vm.$options.name === 'GtrTabsHead') {
+          vm.$children.forEach((childVm) => {
+            if (childVm.$options.name === 'GtrTabsItem' && childVm.name === this.selected) {
+              this.eventBus.$emit('update:selected', this.selected, childVm)
+            }
+          })
+        }
+      })
+    }
+  },
   data () {
     return {
       eventBus: new Vue()
@@ -33,18 +51,8 @@ export default {
     }
   },
   mounted () {
-    if (this.$children.length === 0) {
-      console && console.warn && console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
-    }
-    this.$children.forEach((vm) => {
-      if (vm.$options.name === 'GtrTabsHead') {
-        vm.$children.forEach((childVm) => {
-          if (childVm.$options.name === 'GtrTabsItem' && childVm.name === this.selected) {
-            this.eventBus.$emit('update:selected', this.selected, childVm)
-          }
-        })
-      }
-    })
+    this.checkChildren()
+    this.selectTab()
   }
 }
 </script>
